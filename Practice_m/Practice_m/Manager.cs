ac2c00d8fbc6c29153c75fa5e7a696d7575e9b73
Andrei0903 <krolikakanalturba@mail.ru>
2020-10-17 00:54:28 +0500

    start

---
+++ Practice_m/Practice_m/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Practice_m
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
